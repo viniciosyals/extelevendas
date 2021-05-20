5880c55d36de7d6fd27c295da7727b867867215f
Nikolas Silva <nikolas.rsilva@gmail.com>
2021-05-20 16:23:42 -0300

    WIP: Add redux store boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import { Provider } from 'react-redux';
 import App from './App';
-// import { store } from './store';
+import store from './store';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
